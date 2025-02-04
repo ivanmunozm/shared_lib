1504e35c50b493a6282e9cc0625a5cdf1965eb5b
Ivan Munoz <imunozm.dev@gmail.com>
2025-02-03 22:44:06 -0300

    Quality Gate status

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,3 +1,10 @@
-def call(){
-    waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+def call(String credentialsId) {
+    script {
+        def qualityGate = waitForQualityGate()
+        if (qualityGate.status != 'OK') {
+            error "❌ SonarQube Quality Gate failed: ${qualityGate.status}"
+        } else {
+            echo "✅ SonarQube Quality Gate passed!"
+        }
+    }
 }
