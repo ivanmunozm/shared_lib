6d5abf10a097fdbaf0d642b8c01d39d691b3046e
Ivan Munoz <imunozm.dev@gmail.com>
2025-02-16 22:57:10 -0300

    Se corrige error en dockerbuild

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,8 +1,18 @@
 def call(String project, String ImageTag, String hubUser) {
+    if (!hubUser?.trim()) {
+        error "❌ ERROR: El usuario de Docker Hub (hubUser) no está definido."
+    }
+
     sh """
+       echo "🚀 Construyendo imagen Docker..."
        docker image build -t ${hubUser}/${project} .
+       
+       echo "🏷️ Etiquetando imagen..."
        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+       
+       echo "📤 Subiendo imagen a Docker Hub..."
+       docker login -u ${hubUser} -p ${env.DOCKERHUB_PASSWORD}
        docker image push ${hubUser}/${project}:${ImageTag}
     """
 }
