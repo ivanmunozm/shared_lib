bd0158c63aa4c0aba7a9cde9cf40065756a13579
Ivan Munoz <imunozm.dev@gmail.com>
2025-02-16 23:13:42 -0300

    Se corrige error en dockerbuild

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,18 +1,27 @@
-def call(String project, String ImageTag, String hubUser) {
+def call() {
+    // Obtener parámetros de Jenkins
+    def project = env.ImageName ?: "javappspring"
+    def ImageTag = env.ImageTag ?: "v1"
+    def hubUser = env.DockerHubUser ?: "gusanorock"
+
     if (!hubUser?.trim()) {
         error "❌ ERROR: El usuario de Docker Hub (hubUser) no está definido."
     }
 
-    sh """
-       echo "🚀 Construyendo imagen Docker..."
-       docker image build -t ${hubUser}/${project} .
-       
-       echo "🏷️ Etiquetando imagen..."
-       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-       
-       echo "📤 Subiendo imagen a Docker Hub..."
-       docker login -u ${hubUser} -p ${env.DOCKERHUB_PASSWORD}
-       docker image push ${hubUser}/${project}:${ImageTag}
-    """
+    withCredentials([string(credentialsId: 'DOCKERHUB_PASSWORD', variable: 'DOCKERHUB_PASSWORD')]) {
+        sh """
+           echo "🚀 Construyendo imagen Docker para ${hubUser}/${project}:${ImageTag}..."
+           docker image build -t ${hubUser}/${project} .
+           
+           echo "🏷️ Etiquetando imagen..."
+           docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+           docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+           
+           echo "📤 Autenticando en Docker Hub..."
+           docker login -u ${hubUser} -p ${DOCKERHUB_PASSWORD}
+
+           echo "📤 Subiendo imagen a Docker Hub..."
+           docker image push ${hubUser}/${project}:${ImageTag}
+        """
+    }
 }
