dadde0cafd4fedb992ac2645c693c9f81e9afc1a
Ivan Munoz <imunozm.dev@gmail.com>
2025-02-17 00:08:47 -0300

    Se agrega parameters para reutilizar codigo

--- vars/definePipelineParams.groovy
+++ vars/definePipelineParams.groovy
@@ -1,8 +1,8 @@
 def call() {
-    parameters {
-        choice(name: 'action', choices: ['create', 'delete'], description: 'Choice Action: create/delete')
-        string(name: 'ImageName', description: "Name of the Docker build", defaultValue: 'javappspring')
-        string(name: 'ImageTag', description: "Tag of the Docker build", defaultValue: 'v1')
+    return [
+        choice(name: 'action', choices: ['create', 'delete'], description: 'Choice Action: create/delete'),
+        string(name: 'ImageName', description: "Name of the Docker build", defaultValue: 'javappspring'),
+        string(name: 'ImageTag', description: "Tag of the Docker build", defaultValue: 'v1'),
         string(name: 'DockerHubUser', description: "Docker Hub Username", defaultValue: 'gusanorock')
-    }
+    ]
 }
