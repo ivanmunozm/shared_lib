b126614f01899f900ecd65801f86a5b975798661
Ivan Munoz <imunozm.dev@gmail.com>
2025-02-16 23:26:47 -0300

    Se agrega parameters para reutilizar codigo

--- vars/parameters.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    return [
-        choice(name: 'action', choices: 'create\ndelete', description: 'Choice Action: create/delete'),
-        string(name: 'ImageName', description: "Name of the Docker build", defaultValue: 'javappspring'),
-        string(name: 'ImageTag', description: "Tag of the Docker build", defaultValue: 'v1'),
-        string(name: 'DockerHubUser', description: "Docker Hub Username", defaultValue: 'gusanorock')
-    ]
-}
